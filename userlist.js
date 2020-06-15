667995afc7925857db3f6546806d872168d122cc
Bastien Gounon <45844674+bgounon@users.noreply.github.com>
2020-06-15 16:25:28 +0200

    fix js

--- userlist.js
+++ userlist.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     $.ajax({
+        dataType: "json",
         url: "https://mongolearn.lesondesbros.eu/user/info/testos"
     }).then(function(data) {
-       $('.greeting-id').append(data.Name);
+       $('.greeting-id').append(data.name);
        $('.greeting-content').append(data.City);
     });
 });
