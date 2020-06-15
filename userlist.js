687e3ee4bdd5e2dee4f813e305a12660a614b2c7
Bastien Gounon <45844674+bgounon@users.noreply.github.com>
2020-06-15 16:17:25 +0200

    fix URL

--- userlist.js
+++ userlist.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://learnmongo.lesondesbros.eu/user/info/testos"
+        url: "https://mongolearn.lesondesbros.eu/user/info/testos"
     }).then(function(data) {
        $('.greeting-id').append(data.name);
        $('.greeting-content').append(data.city);
