6bc2bb79a20332f501cd1bb1e03539f74b53fc54
Bastien Gounon <45844674+bgounon@users.noreply.github.com>
2020-06-15 17:05:41 +0200

    fix name

--- userlist.js
+++ userlist.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.getJSON( "https://mongolearn.lesondesbros.eu/user/list", function( data ) {
 
         $.each(data, function(i, value){
-            $(".users-list").append('<li>' + value.id + '</li>');
+            $(".users-list").append('<li>' + value.name + '</li>');
         })
 
     });
