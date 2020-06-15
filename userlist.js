42a76f00483ab236a7a07c294f1edd2c69fcc9b2
Bastien Gounon <45844674+bgounon@users.noreply.github.com>
2020-06-15 15:26:26 +0200

    print name and city in jquery

--- userlist.js
+++ userlist.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.ajax({
         url: "https://learnmongo.lesondesbros.eu/user/info/testos"
     }).then(function(data) {
-       $('.greeting-id').append(data.id);
-       $('.greeting-content').append(data.name);
+       $('.greeting-id').append(data.name);
+       $('.greeting-content').append(data.city);
     });
 });
