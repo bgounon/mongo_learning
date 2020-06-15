c1dce5c103a59059b4c13020cc7e04ea9b00e482
Bastien Gounon <45844674+bgounon@users.noreply.github.com>
2020-06-15 16:19:06 +0200

    uppercase keys

--- userlist.js
+++ userlist.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.ajax({
         url: "https://mongolearn.lesondesbros.eu/user/info/testos"
     }).then(function(data) {
-       $('.greeting-id').append(data.name);
-       $('.greeting-content').append(data.city);
+       $('.greeting-id').append(data.Name);
+       $('.greeting-content').append(data.City);
     });
 });
